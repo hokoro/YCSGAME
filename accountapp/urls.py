d3834645cf9d23c53580c60519868849cd24776b
hokoro <hokoro123@gmail.com>
2021-12-24 07:36:41 +0900

    update 작성 update form 작성 side bar 수정 (account edit 추가)

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from django.views.generic import CreateView
 
-from accountapp.views import AccountCreateView, AccountDetailView
+from accountapp.views import AccountCreateView, AccountDetailView, AccountUpdateView
 
 app_name = 'accountapp'
 
@@ -10,5 +10,6 @@ urlpatterns =[
     path('create/',AccountCreateView.as_view(),name='create'),
     path('login/',LoginView.as_view(template_name='accountapp/login.html'),name='login'),
     path('logout/',LogoutView.as_view(),name='logout'),
-    path('detail/<int:pk>',AccountDetailView.as_view(),name='account'),
+    path('detail/<int:pk>',AccountDetailView.as_view(),name='detail'),
+    path('update/<int:pk>',AccountUpdateView.as_view(),name='update'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DetailView, UpdateView
+
+from accountapp.forms import AccountCreationForm
+from accountapp.decorators import account_ownership_required
 
 
 class AccountCreateView(CreateView):
@@ -13,8 +18,23 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/create.html'
 
+
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
+
+has_ownership = [login_required, account_ownership_required]
+
+
+@method_decorator(has_ownership, 'get')  # 메소드에서 데코레이터를 적용한다 (적용할 데토레이터를 가져와야 한다,어떤 방식의 메소드 를 접근 할지)
+@method_decorator(has_ownership, 'post')  # 애초에 클래스 메소드에서 데코레이터를 작동할수 있는 시스템을 만들어야 한다.
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountCreationForm
+    template_name = 'accountapp/update.html'
+
+    success_url = reverse_lazy('mainapp:mainpage')
+    #def get_success_url(self):
+        #return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
