e9ec3d76b9cbb42e043d6de17379399a87e9617d
hokoro <hokoro123@gmail.com>
2021-12-27 08:54:05 +0900

    delete(계정탈퇴) 기능 작성

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from django.views.generic import CreateView
 
-from accountapp.views import AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp'
 
@@ -12,4 +12,5 @@ urlpatterns =[
     path('logout/',LogoutView.as_view(),name='logout'),
     path('detail/<int:pk>',AccountDetailView.as_view(),name='detail'),
     path('update/<int:pk>',AccountUpdateView.as_view(),name='update'),
+    path('delete/<int:pk>',AccountDeleteView.as_view(),name='delete'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountCreationForm
 from accountapp.decorators import account_ownership_required
@@ -36,5 +36,14 @@ class AccountUpdateView(UpdateView):
     template_name = 'accountapp/update.html'
 
     success_url = reverse_lazy('mainapp:mainpage')
-    #def get_success_url(self):
-        #return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
+    # def get_success_url(self):
+    # return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
+
+
+@method_decorator(has_ownership, 'get')  # 메소드에서 데코레이터를 적용한다 (적용할 데토레이터를 가져와야 한다,어떤 방식의 메소드 를 접근 할지)
+@method_decorator(has_ownership, 'post')  # 애초에 클래스 메소드에서 데코레이터를 작동할수 있는 시스템을 만들어야 한다.
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('mainapp:mainpage')
+    template_name = 'accountapp/delete.html'
