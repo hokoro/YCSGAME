c3c3c065904c8b3c4c58178ab3019b3792cbc465
hokoro <hokoro123@gmail.com>
2021-12-30 13:11:50 +0900

    friendapp 새로 작성 model view url 작성 friend search view 작성 후 검색 하면 사용자 의 profile 과 친구 수락 버튼 메이킹

--- friendapp/urls.py
+++ friendapp/urls.py
@@ -1,7 +1,10 @@
+from django.conf.urls import url
 from django.urls import path
 
+from friendapp.views import FriendSearchView
+
 app_name = 'friendapp'
-
 urlpatterns = [
-
+    path('search/?search=<str:username>',FriendSearchView.as_view(),name='search'),
+    # path('request/(?P<username>[\w.@+-]+)',AddFriend.as_view(),name='request'),
 ]
--- friendapp/views.py
+++ friendapp/views.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import User
+from django.views.generic.detail import SingleObjectMixin, DetailView
+from django.views.generic.list import MultipleObjectMixin
+from rest_framework.views import APIView
 from django.shortcuts import render
+from django.views.generic import ListView
+# Create your views here.
+from friendapp.models import Friend
+from profileapp.models import Profile
 
 
+class FriendSearchView(ListView):
+    model = Profile
+    context_object_name = 'profile_search_result'
+    template_name = 'friendapp/search.html'
+
+    def get_context_data(self,  **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_name = self.request.GET.get('profile_name','')
+        context['profile_search_result'] = Profile.objects.filter(nickname=user_name)
+        return context
+
+# class FriendRequestView(APIView):
+#     def get(self, request, username):
+#         friend = User.objects.get(username=username)
+#         current_user = request.user.username
+#         f = Friend(friend_A=current_user, friend_B=friend, friend_status=1)
+#         f.save()
